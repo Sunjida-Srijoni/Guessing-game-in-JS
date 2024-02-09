89d0da7b5050785714b6075cf45be66b2e9042a2
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-02-09 21:33:33 +0600

    completed guessing game for once

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-parseInt(prompt("Enter a number from 1 to 5"));
+var guessNumber = parseInt(prompt("Enter a number from 1 to 5"));
+
+var randomNUmber = Math.floor(Math.random()*5) + 1;
+
+if(guessNumber === randomNUmber){
+    console.log("you have won");
+
+}
+else{
+    console.log("You have lost. Random number was " + randomNUmber);
+}
