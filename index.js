7944eca49e716e723755ca5129bcd50767bae71a
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-02-09 21:38:38 +0600

    completed guessing game for five times with number of won and lost

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var numbOfWon = 0;
+var numbOfLost = 0;
+
+
 for(var i = 1; i<= 5; i++)
 {
     var guessNumber = parseInt(prompt("Enter a number from 1 to 5"));
@@ -6,10 +10,17 @@ var randomNUmber = Math.floor(Math.random()*5) + 1;
 
 if(guessNumber === randomNUmber){
     console.log("you have won");
+    numbOfWon++;
 
 }
 else{
     console.log("You have lost. Random number was " + randomNUmber);
+    numbOfLost++
 }
 }
+
+document.write("Number of won = " + numbOfWon + "<br>");
+
+document.write("Number of lost = " + numbOfLost + "<br>");
+
 
